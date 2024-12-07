89cbc4c657fdf7ff185c3656db566fe9df3a8663
ebelike9 <ebelike4@gmail.com>
2024-12-07 07:56:30 +0100

    done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default __CONFIG__
+module.exports = {
+  content: [
+  // ...
+  ],
+  theme: {
+  // ...
+  },
+  plugins: [require('daisyui')],
+  };
